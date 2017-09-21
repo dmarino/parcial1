8cfc2ca7e1e138ff55a8861c572f226d4e0579ea
RUTH CECILIA RODRIGUEZ BUITRAGO <d.marino10@uniandes.edu.co>
2017-09-21 09:18:32 -0500

    readMe actualizado

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # parcial1
 Parcial 1 de Web
+
+Salsa Secreta: tener un historial de consultas en mongo.
+Esperanza: crear un arbol en el front que muestre los seguidores. 
--- routes/index.js
+++ routes/index.js
@@ -2,29 +2,29 @@ var express = require('express');
 var router = express.Router();
 const bodyParser= require('body-parser');
 
-//var mongodb = require('mongodb').MongoClient;
-//var db;
-//var url= "mongodb://localhost:27017/test";
+var mongodb = require('mongodb').MongoClient;
+var db;
+var url= "mongodb://dmarino:gabo1234@ds139884.mlab.com:39884/mordorplanet";
 
 var GitHubApi = require("github");
 
-//openMongo();
-//function openMongo(){
-//	mongodb.connect(url, (err, dbs) =>{
-//		if(err){
-//			throw err;
-//		}
-//		console.log("abierto");
-//		db = dbs;
-//	});
-//}
+openMongo();
+function openMongo(){
+	mongodb.connect(url, (err, dbs) =>{
+		if(err){
+			throw err;
+		}
+		console.log("abierto");
+		db = dbs;
+	});
+}
 
 
 router.get('/followers/:user', function(req, resp) {
 
     var github = new GitHubApi({});
 	var user = req.params.user;
-	console.log(user);
+
 
     github.users.getFollowingForUser({
         username: user
@@ -32,5 +32,13 @@ router.get('/followers/:user', function(req, resp) {
     	resp.json(res);
     });
 });
+
+
+router.get('/consultas', function(req, res) {
+	db.collection('parcial').find().toArray(function(err, results) {
+        res.json(results);        
+    })
+});
+
 
 module.exports = router;
